f2980089c6f67d54ef5bd3822fc9b9382af46d65
JoshCodeAligator <joshdebele8@gmail.com>
2025-01-29 19:55:38 -0700

    Configuring Firebase

--- patchnotes/lib/main.dart
+++ patchnotes/lib/main.dart
@@ -2,11 +2,18 @@ import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
